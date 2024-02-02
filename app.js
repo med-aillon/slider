f12447a1254b57e8ec740c96d8a39ad09fd5c4b3
med-aillon <mehdimerghichi@gmail.com>
2024-02-02 19:30:00 +0100

    slide out and lock

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const slides = [...document.querySelectorAll(".slide")];
 
 const sliderData = {
+  locked: false,
   direction: 0,
   slideOutIndex: 0,
   slideInIndex: 0,
@@ -11,8 +12,10 @@ const directionButtons = [...document.querySelectorAll(".direction-btn")];
 directionButtons.forEach((btn) => btn.addEventListener("click", handleClick));
 
 function handleClick(e) {
+  if (sliderData.locked) return;
+  sliderData.locked = true;
   getDirection(e.target);
-  console.log(sliderData);
+  slideOut();
 }
 
 function getDirection(btn) {
@@ -25,4 +28,51 @@ function getDirection(btn) {
   } else {
     sliderData.slideInIndex = sliderData.slideOutIndex + sliderData.direction;
   }
+}
+
+function slideOut() {
+  slideAnimation({
+    el: slides[sliderData.slideInIndex],
+    props: {
+      display: "flex",
+      transform: `translateX(${sliderData.direction < 0 ? "100%" : "-100%"})`,
+      opacity: 0,
+    },
+  });
+
+  slides[sliderData.slideOutIndex].addEventListener("transitionend", slideIn);
+
+  slideAnimation({
+    el: slides[sliderData.slideOutIndex],
+    props: {
+      transition: "transform 0.4s cubic-bezier(0.74, -0.34, 1, 1.19), opacity 0.4s ease-out",
+      transform: `translateX(${sliderData.direction < 0 ? "-100%" : "100%"})`,
+      opacity: 0,
+    },
+  });
+}
+
+function slideAnimation(animationObject) {
+  for (const prop in animationObject.props) {
+    animationObject.el.style[prop] = animationObject.props[prop];
+  }
+}
+
+function slideIn(e) {
+  slideAnimation({
+    el: slides[sliderData.slideInIndex],
+    props: {
+      transition: "transform 0.4s ease-out, opacity 0.6s ease-out",
+      transform: "translateX(0%)",
+      opacity: 1,
+    },
+  });
+  slides[sliderData.slideInIndex].classList.add("active");
+  slides[sliderData.slideOutIndex].classList.remove("active");
+  e.target.removeEventListener("transitionend", slideIn);
+  slides[sliderData.slideOutIndex].style.display = "none";
+
+  setTimeout(() => {
+    sliderData.locked = false;
+  }, 400);
 }
